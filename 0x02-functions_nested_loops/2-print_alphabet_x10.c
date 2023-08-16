7f01087ce3a1173ed15413475c3a526eb4d9848c
Peter Geswint <petergeswint@outlook.com>
2023-08-16 07:12:56 -0700

    updated

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -10,20 +10,16 @@ void print_alphabet_x10(void)
 char ch;
 int i;
 
-i = 0;
-
-while(i < 10)
+while (i < 10)
 {
 ch = 'a';
-	while(ch <= 'z')
-	{
-	_putchar(ch);
-	ch++;
+while (ch <= 'z')
+{
+_putchar(ch);
+ch++;
 
-	
-	}
+}
 _putchar('\n');
 i++;
 }
-
 }
