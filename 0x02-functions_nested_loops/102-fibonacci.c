d43c1049408f2bec7a88593ea78a74bd96a2e17b
Peter Geswint <petergeswint@outlook.com>
2023-08-16 11:05:49 -0700

    Project C low level programming

--- 0x02-functions_nested_loops/102-fibonacci.c
+++ 0x02-functions_nested_loops/102-fibonacci.c
@@ -1,34 +1,32 @@
 #include <stdio.h>
 
+/**
+ * main - prints the first 50 Fibonacci numbers, starting with 1 and 2
+ * followed by a new line
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	long int i, j, k, next;
 
-	/**
-	 * main - Prints first 50 Fibonacci numbers, starting with 1 and 2,
-	 *        separated by a comma followed by a space.
-	 *
-	 * Return: Always 0.
-	 */
-	int main(void)
+	j = 1;
+
+	k = 2;
+
+	for (i = 1; i <= 50; ++i)
 	{
-		int count;
-		unsigned long fib1 = 0, fib2 = 1, sum;
-
-
-		for (count = 0; count < 50; count++)
+		if (j != 20365011074)
 		{
-			sum = fib1 + fib2;
-			printf("%lu", sum);
-
-
-			fib1 = fib2;
-			fib2 = sum;
-
-
-			if (count == 49)
-				printf("\n");
-			else
-				printf(", ");
+			printf("%ld, ", j);
+		} else
+		{
+			printf("%ld\n", j);
 		}
-
-
-		return (0);
+		next = j + k;
+		j = k;
+		k = next;
 	}
+
+	return (0);
+}
+
