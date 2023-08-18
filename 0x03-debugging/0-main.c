04eef708d3494cca39255e6dd31caa825bc56ded
Peter Geswint <petergeswint@outlook.com>
2023-08-18 14:30:27 -0700

    added blank line

--- 0x03-debugging/0-main.c
+++ 0x03-debugging/0-main.c
@@ -1,14 +1,16 @@
 #include "main.h"
 
 /**
- * main returns function that prints if integer is positive or negative
- * Return: 0
- */
+*main - Test function that prints if integer is positive or negative
+*Return: 0
+*/
 
 int main(void)
 {
 	int i;
+
 	i = 0;
+
 	positive_or_negative(i);
-	return(0);
+	return (0);
 }
