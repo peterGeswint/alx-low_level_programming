4412461f9d1b9f854f74106fdc53f8b4a206b0a2
Peter Geswint <petergeswint@outlook.com>
2023-08-18 11:51:16 -0700

    Debugging

---
+++ 0x03-debugging/1-main.c
@@ -0,0 +1,17 @@
+#include <studio.h>
+/**
+ * main -causes an infinite loop
+ * Return: 0
+ */
+int main(void)
+{
+int i;
+print("Infinite loop incomming :\n");
+i = 0;
+/*while(i <10)*/
+/*{*/
+/*putchar(i);*/
+/*}*/
+printf("Infinite loop avoided! \\o/\n");
+return (0);
+}
