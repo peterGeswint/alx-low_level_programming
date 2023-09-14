e045c709a8259553ada0b597ba9ae730c5951e66
Peter Geswint <petergeswint@outlook.com>
2023-09-13 18:25:58 -0700

    3-op_function.c

--- 0x0F-function_pointers/3-op_functions.c
+++ 0x0F-function_pointers/3-op_functions.c
@@ -1,11 +1,11 @@
 #include "3-calc.h"
 
+
 int op_add(int a, int b);
 int op_sub(int a, int b);
 int op_mul(int a, int b);
 int op_div(int a, int b);
 int op_mod(int a, int b);
-
 
 /**
  * op_add - Returns the sum of two numbers.
@@ -58,8 +58,7 @@ int op_div(int a, int b)
  *
  * Return: The remainder of the division of a by b.
  */
-	int op_mod(int a, int b)
+int op_mod(int a, int b)
 {
 	return (a % b);
-	
 }
