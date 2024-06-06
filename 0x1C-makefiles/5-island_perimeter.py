deeb154eb95d35e95b27c3ac67b1c45e334f5171
peterGeswint <petergeswint@outlook.com>
2024-06-06 11:56:22 -0700

    task 5

--- 0x1C-makefiles/5-island_perimeter.py
+++ 0x1C-makefiles/5-island_perimeter.py
@@ -7,14 +7,15 @@ def island_perimeter(grid):
 
     height = len(grid)
     width = len(grid[0])
-    perimeter = 0
+    size = 0
+    edges = 0
 
     for i in range(height):
         for j in range(width):
             if grid[i][j] == 1:
-                perimeter += 4
-                if i > 0 and grid[i-1][j] == 1:  # Check cell above
-                    perimeter -= 2
-                if j > 0 and grid[i][j-1] == 1:  # Check cell to the left
-                    perimeter -= 2
-    return perimeter
+                size += 1
+                if (j > 0 and grid[i][j - 1] == 1):
+                    edges += 1
+                if (i > 0 and grid[i - 1][j] == 1):
+                    edges += 1
+    return ((size * 4) - (edges * 2))
