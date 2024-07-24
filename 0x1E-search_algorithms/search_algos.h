9694963ac8a0bee71d618e2cedd2de36f931d6a7
peterGeswint <petergeswint@outlook.com>
2024-07-24 13:12:45 +0200

    task 1

--- 0x1E-search_algorithms/search_algos.h
+++ 0x1E-search_algorithms/search_algos.h
@@ -4,6 +4,7 @@
 #include <stdio.h>
 #include <stdlib.h>
 
+int binary_search(int *array, size_t size, int value);
 int linear_search(int *array, size_t size, int value);
 
 #endif /*__SEARCH_ALGOS_H_*/
